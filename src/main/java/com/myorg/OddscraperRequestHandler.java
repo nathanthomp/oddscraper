e437347f94697db47f097ff51eae52b1d87089d6
Nathan Thompson <thompn18@email.franklin.edu>
2025-02-23 11:47:56 -0500

    Add OddscraperRequestHandler.java

---
+++ src/main/java/com/myorg/OddscraperRequestHandler.java
@@ -0,0 +1,20 @@
+package com.myorg;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class OddscraperRequestHandler implements RequestHandler<Request, Response> {
+
+    @Override
+    public Response handleRequest(Request request, Context context) {
+
+        return new Response("Hello, world!");
+    }
+
+}
+
+record Request(String content) {
+}
+
+record Response(String content) {
+}
