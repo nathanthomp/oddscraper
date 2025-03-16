3db65d40940bb6d5b0883f21752ee9ad1d971398
Nathan Thompson <thompn18@email.franklin.edu>
2025-03-16 16:05:15 -0400

    Add scraper clients

---
+++ src/main/java/com/nathanthomp/oddscraper/scrapers/ScraperHttpClient.java
@@ -0,0 +1,37 @@
+package com.nathanthomp.oddscraper.scrapers;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandler;
+
+public class ScraperHttpClient {
+    private static ScraperHttpClient instance = null;
+
+    public static ScraperHttpClient getInstance() {
+        if (instance == null) {
+            instance = new ScraperHttpClient();
+        }
+        return instance;
+    }
+
+    private HttpClient httpClient;
+
+    private ScraperHttpClient() {
+        this.httpClient = HttpClient.newHttpClient();
+    }
+
+    public HttpResponse<String> getHttpResponse(String endpoint) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endpoint)).GET().build();
+
+        BodyHandler<String> bodyHandler = HttpResponse.BodyHandlers.ofString();
+        return httpClient.send(request, bodyHandler);
+    }
+
+    public String getHttpResonseHeader(HttpResponse<String> response, String key) {
+        HttpHeaders headers = response.headers();
+        return headers.map().get(key).getFirst();
+    }
+}
---
+++ src/main/java/com/nathanthomp/oddscraper/scrapers/ScraperSecretsManagerClient.java
@@ -0,0 +1,59 @@
+package com.nathanthomp.oddscraper.scrapers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.internal.LinkedTreeMap;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+
+public class ScraperSecretsManagerClient {
+    private static ScraperSecretsManagerClient instance = null;
+
+    public static ScraperSecretsManagerClient getInstance() {
+        if (instance == null) {
+            instance = new ScraperSecretsManagerClient();
+        }
+        return instance;
+    }
+
+    private final String SECRET_NAME = "OddscraperSecretsManager56C-45zSscfjucq3";
+    private final Region SECRET_REGION = Region.of("us-east-2");
+
+    private Map<String, String> secrets;
+
+    public ScraperSecretsManagerClient() {
+        secrets = new HashMap<String, String>();
+
+        SecretsManagerClient secretsClient = SecretsManagerClient.builder()
+                .region(SECRET_REGION)
+                .build();
+
+        GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
+                .secretId(SECRET_NAME)
+                .build();
+
+        String secretString = secretsClient.getSecretValue(getSecretValueRequest).secretString();
+
+        Gson gson = new Gson();
+        LinkedTreeMap<String, String> secrets = (LinkedTreeMap) gson.fromJson(secretString, Object.class);
+
+        for (Map.Entry<String, String> secretEntry : secrets.entrySet()) {
+            this.secrets.put(secretEntry.getKey(), secretEntry.getValue());
+        }
+
+    }
+
+    public Map<String, String> getOddsApiSecrets() {
+        Map<String, String> result = new HashMap<String, String>();
+        for (Map.Entry<String, String> mapEntry : this.secrets.entrySet()) {
+            if (mapEntry.getKey().startsWith("OddsApiKey")) {
+                result.put(mapEntry.getKey(), mapEntry.getValue());
+            }
+        }
+        return result;
+    }
+}
