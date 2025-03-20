dc05986d271651d442384eb83496456d4b0e8ec8
Nathan Thompson <thompn18@email.franklin.edu>
2025-03-19 21:17:44 -0400

    Testing updates

---
+++ src/test/java/com/nathanthomp/oddscraper/OddListTest.java
@@ -0,0 +1,81 @@
+package com.nathanthomp.oddscraper;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.nathanthomp.oddscraper.odds.Odd;
+import com.nathanthomp.oddscraper.odds.OddLeague;
+import com.nathanthomp.oddscraper.odds.OddList;
+import com.nathanthomp.oddscraper.odds.OddMarket;
+import com.nathanthomp.oddscraper.scrapers.OddsApiScraper;
+
+public class OddListTest {
+
+    @Test
+    public void testAddOdd_NotEmpty() {
+
+        String path = "data/odds-api-response-odds-cbb-moneyline.json";
+        OddList oddList = new OddList();
+        List<Odd> odds;
+        try {
+            odds = OddsApiScraper.scrapeOddsFromFile(path, OddLeague.ODDLEAGUE_CBB, OddMarket.ODDMARKET_MONEYLINE);
+        } catch (Exception e) {
+            fail("Could not get data from file: " + e.getMessage());
+            return;
+        }
+
+        for (Odd odd : odds) {
+            oddList.addOdd(odd);
+        }
+
+        assertTrue(!oddList.getOdds().isEmpty());
+    }
+
+    @Test
+    public void testGetBestOdds_NotEmpty() {
+
+        String path = "data/odds-api-response-odds-cbb-moneyline.json";
+        OddList oddList = new OddList();
+        List<Odd> odds;
+        try {
+            odds = OddsApiScraper.scrapeOddsFromFile(path, OddLeague.ODDLEAGUE_CBB, OddMarket.ODDMARKET_MONEYLINE);
+        } catch (Exception e) {
+            fail("Could not get data from file: " + e.getMessage());
+            return;
+        }
+
+        for (Odd odd : odds) {
+            oddList.addOdd(odd);
+        }
+
+        OddList bestOdds = oddList.getBestOdds();
+
+        assertTrue(!bestOdds.getOdds().isEmpty());
+    }
+
+    @Test
+    public void testToString_NotEmpty() {
+
+        String path = "data/odds-api-response-odds-cbb-moneyline.json";
+        OddList oddList = new OddList();
+        List<Odd> odds;
+        try {
+            odds = OddsApiScraper.scrapeOddsFromFile(path, OddLeague.ODDLEAGUE_CBB, OddMarket.ODDMARKET_MONEYLINE);
+        } catch (Exception e) {
+            fail("Could not get data from file: " + e.getMessage());
+            return;
+        }
+
+        for (Odd odd : odds) {
+            oddList.addOdd(odd);
+        }
+
+        OddList bestOdds = oddList.getBestOdds();
+
+        assertTrue(!bestOdds.toString().isEmpty());
+    }
+}
--- src/test/java/com/nathanthomp/oddscraper/OddscraperRequestHandlerTest.java
+++ src/test/java/com/nathanthomp/oddscraper/OddscraperRequestHandlerTest.java
@@ -3,6 +3,9 @@ package com.nathanthomp.oddscraper;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 
 import com.amazonaws.services.lambda.runtime.Context;
@@ -11,27 +14,77 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 
 public class OddscraperRequestHandlerTest {
 
-    @Test
-    public void testHandleRequest_StatusCode() {
-        OddscraperRequestHandler handler = new OddscraperRequestHandler();
+        @Test
+        public void testHandleRequest_StatusCode500_MissingRequiredLeaugeRequestParameter() {
+                Context context = mock(Context.class);
+                APIGatewayProxyRequestEvent request = new APIGatewayProxyRequestEvent();
 
-        Context context = mock(Context.class);
-        APIGatewayProxyRequestEvent request = new APIGatewayProxyRequestEvent();
-        APIGatewayProxyResponseEvent response = handler.handleRequest(request,
-                context);
+                Map<String, String> queryParameters = new HashMap<String, String>();
+                queryParameters.put("market", "moneyline");
 
-        assertTrue(response.getStatusCode() == 200);
-    }
+                request.setQueryStringParameters(queryParameters);
 
-    // @Test
-    // public void testHandleRequest_Body() {
-    // OddscraperRequestHandler handler = new OddscraperRequestHandler();
+                OddscraperRequestHandler handler = new OddscraperRequestHandler();
+                APIGatewayProxyResponseEvent response = handler.handleRequest(request,
+                                context);
 
-    // Context context = mock(Context.class);
-    // APIGatewayProxyRequestEvent request = new APIGatewayProxyRequestEvent();
-    // APIGatewayProxyResponseEvent response = handler.handleRequest(request,
-    // context);
+                assertTrue(response.getStatusCode() == 500
+                                && response.getBody().contains("Missing required 'league' request parameter"));
+        }
 
-    // assertTrue(response.getBody().equals("{\"msg\":\"Hello, AWS!\"}"));
-    // }
+        @Test
+        public void testHandleRequest_StatusCode500_MissingRequiredMarketRequestParameter() {
+                Context context = mock(Context.class);
+                APIGatewayProxyRequestEvent request = new APIGatewayProxyRequestEvent();
+
+                Map<String, String> queryParameters = new HashMap<String, String>();
+                queryParameters.put("league", "ucl");
+
+                request.setQueryStringParameters(queryParameters);
+
+                OddscraperRequestHandler handler = new OddscraperRequestHandler();
+                APIGatewayProxyResponseEvent response = handler.handleRequest(request,
+                                context);
+
+                assertTrue(response.getStatusCode() == 500
+                                && response.getBody().contains("Missing required 'market' request parameter"));
+        }
+
+        @Test
+        public void testHandleRequest_StatusCode500_InvalidLeagueRequestParameter() {
+                Context context = mock(Context.class);
+                APIGatewayProxyRequestEvent request = new APIGatewayProxyRequestEvent();
+
+                Map<String, String> queryParameters = new HashMap<String, String>();
+                queryParameters.put("league", "something");
+                queryParameters.put("market", "moneyline");
+
+                request.setQueryStringParameters(queryParameters);
+
+                OddscraperRequestHandler handler = new OddscraperRequestHandler();
+                APIGatewayProxyResponseEvent response = handler.handleRequest(request,
+                                context);
+
+                assertTrue(response.getStatusCode() == 500
+                                && response.getBody().contains("Invalid 'league' request parameter:"));
+        }
+
+        @Test
+        public void testHandleRequest_StatusCode500_InvalidMarketRequestParameter() {
+                Context context = mock(Context.class);
+                APIGatewayProxyRequestEvent request = new APIGatewayProxyRequestEvent();
+
+                Map<String, String> queryParameters = new HashMap<String, String>();
+                queryParameters.put("league", "ucl");
+                queryParameters.put("market", "something");
+
+                request.setQueryStringParameters(queryParameters);
+
+                OddscraperRequestHandler handler = new OddscraperRequestHandler();
+                APIGatewayProxyResponseEvent response = handler.handleRequest(request,
+                                context);
+
+                assertTrue(response.getStatusCode() == 500
+                                && response.getBody().contains("Invalid 'market' request parameter:"));
+        }
 }
